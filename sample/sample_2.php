3900ef53baa6a52915745def009bd005dd82694a
muroi <intemous9@gmail.com>
2016-07-06 11:34:06 +0900

    added sample_2.php

--- sample/sample_2.php
+++ sample/sample_2.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * シンプルなエクセルを作成
- * php sample_1.php
+ * シートを複数持ったエクセルを作成
+ * php sample_2.php
  */
 
 date_default_timezone_set('Asia/Tokyo');
@@ -28,6 +28,18 @@ $sheet->setCellValue('A2', 100);
 $sheet->setCellValue('B2', 200);
 $sheet->setCellValue('C2', 300);
 
+// 2シート目を作成
+$sheet2 = $book->createSheet();
+$sheet2->setTitle('test_sheet2');
+
+// セル指定で値を設定
+$sheet2->setCellValue('A1', 'hoge2');
+$sheet2->setCellValue('B1', 'huga2');
+$sheet2->setCellValue('C1', 'piyo2');
+$sheet2->setCellValue('A2', 100);
+$sheet2->setCellValue('B2', 200);
+$sheet2->setCellValue('C2', 300);
+
 // 出力
 $writer = PHPExcel_IOFactory::createWriter($book, 'Excel2007');
-$writer->save('output/sample_1.xlsx');
+$writer->save('output/sample_2.xlsx');
